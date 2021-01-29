45afc03dd9728183f5cb5a2b6f99c715bebbb0a8
Aarti <aarti.gusain1999@gmail.com>
2021-01-29 20:32:10 +0530

    adding MavenHelloWorld project

---
+++ MavenHelloWorld/src/main/java/mavenhello/test/HelloAarti.java
@@ -0,0 +1,10 @@
+package mavenhello.test;
+
+public class HelloAarti {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Aarti");
+	}
+
+}
